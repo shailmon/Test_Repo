0cabbec3122c589d36eb7ab12b6398c2741635d2
shailmon <shailesh.monica24@gmail.com>
2021-04-01 19:26:05 +0530

    first

--- RestAssured_Test/src/test/java/TestRest.java
+++ RestAssured_Test/src/test/java/TestRest.java
@@ -9,9 +9,15 @@ public class TestRest {
 		
 		
 		JsonSchemaValidator validator = JsonSchemaValidator.matchesJsonSchemaInClasspath("schema.json");
-		given().when().get("https://bookstore.toolsqa.com/BookStore/v1/Books").then().body(validator);
+		String resp = given().when().get("https://bookstore.toolsqa.com/BookStore/v1/Books").then().body(validator).extract().asString();
 		
-		
+		if(validator.matches(resp)) {
+			
+			System.out.println("Schema Validation Passed!");
+			
+		}
+		else
+			System.out.println("Schema Validation Failed!");
 				
 		
 		
